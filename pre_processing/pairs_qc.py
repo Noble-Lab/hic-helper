e73f4c59dd50d1b8002ebd8161fd5b114aa41a60
Xiao Wang <wang3702@purdue.edu>
2024-05-01 19:24:30 +0800

    fix pairsqc calling typos

--- pre_processing/pairs_qc.py
+++ pre_processing/pairs_qc.py
@@ -29,11 +29,12 @@ if __name__ == '__main__':
     output_dir = os.path.abspath(output_dir)
     os.makedirs(output_dir, exist_ok=True)
     script_dir = os.path.dirname(os.path.realpath(__file__))
+    prefix ="4DN_QC"
     script_dir = script_dir.replace('pre_processing','bin')
     script_dir = os.path.join(script_dir, "pairsqc")
     script_path = os.path.join(script_dir, 'pairsqc.py')
     output_prefix = os.path.join(output_dir,"4DN_QC")
-    os.system(f'python3 {script_path} -p {input_pairs} -c {chromsize_file} -tP -s {output_prefix} -O {output_prefix}')
+    os.system(f'python3 {script_path} -p {input_pairs} -c {chromsize_file} -tP -s {prefix} -O {output_prefix}')
     #python3 $scriptdir/pairsqc.py -p $input_pairs -c $chromsize -tP -s $sample_name -O $sample_name
 
     script_path = os.path.join(script_dir, 'plot.r')
