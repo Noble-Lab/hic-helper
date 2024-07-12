3bc940490042dbfc959b60ef060759061eaa399c
Xiao Wang <wang3702@purdue.edu>
2024-07-11 19:41:22 -0700

    update on bigwig2array to avoid stat bug

--- pre_processing/bigwig2array.py
+++ pre_processing/bigwig2array.py
@@ -11,27 +11,27 @@ def bigwig2array(input_bw, output_pkl, resolution):
         print("Processing", chrom)
         chrom_size = chroms[chrom]
         print("Chrom size:", chrom_size)
-        # signal = bw.values(chrom, 0, chrom_size, numpy=True)
+        signal = bw.values(chrom, 0, chrom_size, numpy=True)
         #each resolution interval should sum to get the overall signal
         cutoff_length = chrom_size // resolution * resolution
         if cutoff_length == 0:
             print("Chrom size is smaller than resolution, skipping")
             continue
-        # signal = signal[:cutoff_length]
+        signal = signal[:cutoff_length]
         
-        # signal = np.nan_to_num(signal)
-        # if len(signal)==0:
-        #     print("Empty signal for", chrom)
-        #     continue
-        # if resolution > 1:
-        #     signal = signal.reshape(-1, resolution).mean(axis=1)
-        # signal_dict[chrom] = signal
-        value_list = []
-        for i in list(range(0, cutoff_length, resolution)):
-            value_list.append(bw.stats(chrom, i, i + resolution)[0])
+        signal = np.nan_to_num(signal)
+        
+        if resolution > 1:
+            signal = signal.reshape(-1, resolution).mean(axis=1)
+        signal_dict[chrom] = signal
 
-        value_list = [0 if v is None else v for v in value_list]
-        signal = np.array(value_list)
+        # the following problem is nan will have very big impact on overall calculation
+        # value_list = []
+        # for i in list(range(0, cutoff_length, resolution)):
+        #     value_list.append(bw.stats(chrom, i, i + resolution)[0])
+
+        # value_list = [0 if v is None else v for v in value_list]
+        # signal = np.array(value_list)
 
         signal_dict[chrom] = signal
         print("Finished procssing! Signal shape:", signal.shape)
